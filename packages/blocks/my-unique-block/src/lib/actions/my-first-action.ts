a0fe298a890bb9f74bf0d1d22957e5dff34f706d
Jura Gorohovsky <gorohoroh@gmail.com>
2025-10-03 18:03:03 +0000

    Sample action payload to make a request to httpbin

--- packages/blocks/my-unique-block/src/lib/actions/my-first-action.ts
+++ packages/blocks/my-unique-block/src/lib/actions/my-first-action.ts
@@ -1,4 +1,5 @@
 import { createAction, Property } from '@openops/blocks-framework';
+import { httpClient, HttpMethod } from '@openops/blocks-common';
 
 export const myFirstAction = createAction({
   requireToolApproval: false,
@@ -6,7 +7,11 @@ export const myFirstAction = createAction({
   displayName: 'My first action',
   description: 'What this action does',
   props: {},
-  async run() {
-    // Action logic here
+  async run(context) {
+    const res = await httpClient.sendRequest<string[]>({
+      method: HttpMethod.GET,
+      url: 'https://httpbin.org/get',
+    });
+    return res.body;
   },
 });
