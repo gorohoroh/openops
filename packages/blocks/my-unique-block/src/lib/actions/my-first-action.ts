29c29b0ba9465f98f042b4e9180704e3febfc12e
Jura Gorohovsky <gorohoroh@gmail.com>
2025-10-03 17:55:57 +0000

    Adding a required createAction({}) parameter that scaffolding didn't add

--- packages/blocks/my-unique-block/src/lib/actions/my-first-action.ts
+++ packages/blocks/my-unique-block/src/lib/actions/my-first-action.ts
@@ -1,6 +1,7 @@
 import { createAction, Property } from '@openops/blocks-framework';
 
 export const myFirstAction = createAction({
+  requireToolApproval: false,
   name: 'myFirstAction',
   displayName: 'My first action',
   description: 'What this action does',
